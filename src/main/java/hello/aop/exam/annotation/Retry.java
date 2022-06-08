e3bf75654a6e711404a7f156212e2b33f5bd0eac
woosunghwan <wsh0821@station3.co.kr>
2022-06-08 16:08:03 +0900

    강의 종강

---
+++ src/main/java/hello/aop/exam/annotation/Retry.java
@@ -0,0 +1,15 @@
+package hello.aop.exam.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface Retry {
+
+    int value() default 3;
+
+}
--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -16,7 +16,8 @@ import org.springframework.context.annotation.Import;
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
 //@Import(AspectV4Pointcut.class)
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class}) // 어드바이스 순서는 @Aspect 클래스 기준으로 동작한다.
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class}) // 어드바이스 순서는 @Aspect 클래스 기준으로 동작한다.
+@Import(AspectV6Advice.class)
 @SpringBootTest
 public class AopTest {
 
